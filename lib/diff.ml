acad77b538db1822dab9e704afe82bc141622180
Marcel Ndowah <ndowahmarcel@gmail.com>
2024-10-09 08:30:43 +0100

    define the class type

--- lib/diff.ml
+++ lib/diff.ml
@@ -4,26 +4,27 @@ type ('item, 'diff) t = Added of 'item | Removed of 'item | Modified of 'diff
 
 type 'a atomic_modification = { reference : 'a; current : 'a }
 (** The simplest diff representation for the modification of a value of type 'a.
-     [reference] is the value before and [current] is the value after the change occurred.
+     [reference] is the value before and [current] is the value after the change occured.
      Use this type when there is no better representation available. *)
-
 type value = {
   vname : string;
   vdiff : (value_description, value_description atomic_modification) t;
 }
-type class_ = {
-  cname: string;
-  cdiff: (class_declaration atomic_modification) t;
-}
+
 type module_ = {
   mname : string;
   mdiff : (module_declaration, module_modification) t;
 }
+
 and module_modification = Unsupported | Supported of sig_item list
+type class = {
+  cname : string;
+  cdiff : (class_declaration, class_modification)
+}
 and sig_item = Value of value | Module of module_
 
-type item_type = Value_item | Module_item | Class_item [@@deriving ord]
-type sig_items = Val of value_description | Mod of module_declaration | Cls of class_declaration
+type item_type = Value_item | Module_item [@@deriving ord]
+type sig_items = Val of value_description | Mod of module_declaration
 
 module Sig_item_map = Map.Make (struct
   type t = item_type * string [@@deriving ord]
@@ -37,15 +38,46 @@ let extract_items items =
           Sig_item_map.add (Module_item, Ident.name id) (Mod mod_decl) tbl
       | Sig_value (id, val_des, _) ->
           Sig_item_map.add (Value_item, Ident.name id) (Val val_des) tbl
-      | Sig_class (id, cls_decl, _, _) ->
-          Sig_item_map.add (Class_item, Ident.name id) (Cls cls_decl) tbl
       | _ -> tbl)
     Sig_item_map.empty items
 
-let print_class_change = function
-  | Added cls_name -> Printf.printf "Added class: %s\n" cls_name
-  | Removed cls_name -> Printf.printf "Removed class: %s\n" cls_name
-  | _ -> ()
+let modtype_item ~loc ~typing_env ~name ~reference ~current =
+  let modtype_coercion1 () =
+    Includemod.modtypes ~loc typing_env ~mark:Mark_both reference current
+  in
+  let modtype_coercion2 () =
+    Includemod.modtypes ~loc typing_env ~mark:Mark_both current reference
+  in
+  match (modtype_coercion1 (), modtype_coercion2 ()) with
+  | Tcoerce_none, Tcoerce_none -> None
+  | _, _ -> Some (Module { mname = name; mdiff = Modified Unsupported })
+  | exception Includemod.Error _ ->
+      Some (Module { mname = name; mdiff = Modified Unsupported })
+
+let value_item ~typing_env ~name ~reference ~current =
+  match (reference, current) with
+  | None, None -> None
+  | Some (Val reference), None ->
+      Some (Value { vname = name; vdiff = Removed reference })
+  | None, Some (Val current) ->
+      Some (Value { vname = name; vdiff = Added current })
+  | Some (Val reference), Some (Val current) -> (
+      let val_coercion1 () =
+        Includecore.value_descriptions ~loc:current.val_loc typing_env name
+          current reference
+      in
+      let val_coercion2 () =
+        Includecore.value_descriptions ~loc:reference.val_loc typing_env name
+          reference current
+      in
+      match (val_coercion1 (), val_coercion2 ()) with
+      | Tcoerce_none, Tcoerce_none -> None
+      | _, _ ->
+          Some (Value { vname = name; vdiff = Modified { reference; current } })
+      | exception Includecore.Dont_match _ ->
+          Some (Value { vname = name; vdiff = Modified { reference; current } })
+      )
+  | _ -> None
 
 let rec items ~reference ~current =
   let env = Typing_env.for_diff ~reference ~current in
@@ -57,16 +89,7 @@ let rec items ~reference ~current =
       | Value_item, reference, current ->
           value_item ~typing_env:env ~name ~reference ~current
       | Module_item, reference, current ->
-          module_item ~typing_env:env ~name ~reference ~current
-      | Class_item, None, Some (Cls curr_cls) ->
-          print_class_change (Added (Ident.name curr_cls));
-          Some (Class { cname = name; cdiff = Added curr_cls })
-      | Class_item, Some (Cls ref_cls), None ->
-          print_class_change (Removed (Ident.name ref_cls));
-          Some (Class { cname = name; cdiff = Removed ref_cls })
-      | Class_item, Some (Cls reference), Some (Cls current) ->
-          class_declaration ~typing_env:env ~name ~reference ~current
-      | _ -> None)
+          module_item ~typing_env:env ~name ~reference ~current)
     ref_items curr_items
   |> Sig_item_map.bindings |> List.map snd
 
@@ -89,24 +112,6 @@ and module_declaration ~typing_env ~name ~reference ~current =
   | ref_modtype, curr_modtype ->
       modtype_item ~loc:reference.md_loc ~typing_env ~name
         ~reference:ref_modtype ~current:curr_modtype
-
-and class_item ~typing_env ~name ~reference ~current =
-  match (reference, current) with
-  | None, None -> None
-  | None, Some (Cls curr_cls) ->
-      print_class_change (Added (Ident.name curr_cls));
-      Some (Class { cname = name; cdiff = Added curr_cls })
-  | Some (Cls ref_cls), None ->
-      print_class_change (Removed (Ident.name ref_cls));
-      Some (Class { cname = name; cdiff = Removed ref_cls })
-  | Some (Cls reference), Some (Cls current) ->
-      class_declaration ~typing_env ~name ~reference ~current
-  | _ -> assert false
-
-and class_declaration ~typing_env ~name ~reference ~current =
-  match Includemod.classes typing_env reference current with
-  | Tcoerce_none -> None
-  | _ -> Some (Class { cname = name; cdiff = Modified { reference; current } })
 
 and signatures ~typing_env ~reference ~current =
   match items ~reference ~current with
